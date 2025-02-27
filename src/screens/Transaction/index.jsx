865950e54977e96302623f861daace0496211bc9
diogo <diogobrasildasilva@gmail.com>
2025-02-27 15:56:59 -0300

    refactor: add scroll  na tela de transação

--- src/screens/Transaction/index.jsx
+++ src/screens/Transaction/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, TextInput, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Container, Header, Title, Input, ButtonContainer, ActionButton, ButtonText, DateInputContainer, DateInputLabel, DateInput, SelectBoxContainer, SelectBoxView, SelectBoxLabel, SubmitButton, SubmitButtonText } from './styles';
 import IncomeIcon from '../../../assets/Incom.svg';
 import OutcomeIcon from '../../../assets/Outcome.svg';
@@ -42,92 +43,94 @@ export const Transaction = () => {
   };
 
   return (
-    <Container>
-      <Header>
-        <Title>Transação</Title>
-      </Header>
-      <Input
-        placeholder="Valor"
-        marginTop={`${hp('22%')}px`}
-        keyboardType="numeric"
-      />
-      <DateInputContainer>
-        <DateInputLabel>Data da Transação</DateInputLabel>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <DateInput
-            placeholder="YYYY-MM-DD"
-            value={date}
-            onChangeText={handleDateChange}
-            onBlur={handleDateBlur}
-            keyboardType="numeric"
-          />
-          <Icon name="calendar" size={24} color="#000" style={{ marginLeft: 10 }} />
-        </View>
-      </DateInputContainer>
-      <ButtonContainer>
-        <ActionButton
-          onPress={() => handleTransactionTypeChange('income')}
-          selected={transactionType === 'income'}
-        >
-          <IncomeIcon width={24} height={24} />
-          <ButtonText selected={transactionType === 'income'}>
-            Entrada
-          </ButtonText>
-        </ActionButton>
-        <ActionButton
-          onPress={() => handleTransactionTypeChange('outcome')}
-          selected={transactionType === 'outcome'}
-        >
-          <OutcomeIcon width={24} height={24} />
-          <ButtonText selected={transactionType === 'outcome'}>
-            Saída
-          </ButtonText>
-        </ActionButton>
-      </ButtonContainer>
-      <SelectBoxContainer>
-        <SelectBoxLabel>Categoria</SelectBoxLabel>
-        <SelectBoxView>
-          <Picker
-            selectedValue={'defult'}
-            onValueChange={(itemValue, itemIndex) => console.log(itemValue)}
+    <KeyboardAwareScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <Container>
+        <Header>
+          <Title>Transação</Title>
+        </Header>
+        <Input
+          placeholder="Valor"
+          marginTop={`${hp('22%')}px`} // 153px (Header height) + 27px
+          keyboardType="numeric"
+        />
+        <DateInputContainer>
+          <DateInputLabel>Data da Transação</DateInputLabel>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <DateInput
+              placeholder="YYYY-MM-DD"
+              value={date}
+              onChangeText={handleDateChange}
+              onBlur={handleDateBlur}
+              keyboardType="numeric"
+            />
+            <Icon name="calendar" size={24} color="#000" style={{ marginLeft: 10 }} />
+          </View>
+        </DateInputContainer>
+        <ButtonContainer>
+          <ActionButton
+            onPress={() => handleTransactionTypeChange('income')}
+            selected={transactionType === 'income'}
           >
-            <Picker.Item label="Comida" value="food" />
-            <Picker.Item label="Transporte" value="transport" />
-            <Picker.Item label="Saúde" value="health" />
-            <Picker.Item label="Educação" value="education" />
-            <Picker.Item label="Entretenimento" value="entertainment" />
-            <Picker.Item label="Compras" value="shopping" />
-            <Picker.Item label="Outros" value="others" />
-            <Picker.Item label="Salário" value="salary" />
-            <Picker.Item label="Freelance" value="freelance" />
-            <Picker.Item label="Investimentos" value="investments" />
-            <Picker.Item label="Negócios" value="business" />
-            <Picker.Item label="Outros Rendimentos" value="other_income" />
-          </Picker>
-        </SelectBoxView>
-      </SelectBoxContainer>
-      <SelectBoxContainer>
-        <SelectBoxLabel>Carteira</SelectBoxLabel>
-        <SelectBoxView>
-          <Picker
-            selectedValue={'defult'}
-            onValueChange={(itemValue, itemIndex) => console.log(itemValue)}
+            <IncomeIcon width={24} height={24} />
+            <ButtonText selected={transactionType === 'income'}>
+              Entrada
+            </ButtonText>
+          </ActionButton>
+          <ActionButton
+            onPress={() => handleTransactionTypeChange('outcome')}
+            selected={transactionType === 'outcome'}
           >
-            <Picker.Item label="Nubank" value="1" />
-            <Picker.Item label="Mercado Pago" value="2" />
-            <Picker.Item label="Inter" value="3" />
-            <Picker.Item label="PicPay" value="4" />
-            <Picker.Item label="Banco do Brasil" value="5" />
-          </Picker>
-        </SelectBoxView>
-      </SelectBoxContainer>
-      <Input
-        placeholder="Descrição"
-        marginTop={`${hp('3%')}px`}
-      />
-      <SubmitButton>
-        <SubmitButtonText>Enviar</SubmitButtonText>
-      </SubmitButton>
-    </Container>
+            <OutcomeIcon width={24} height={24} />
+            <ButtonText selected={transactionType === 'outcome'}>
+              Saída
+            </ButtonText>
+          </ActionButton>
+        </ButtonContainer>
+        <SelectBoxContainer>
+          <SelectBoxLabel>Categoria</SelectBoxLabel>
+          <SelectBoxView>
+            <Picker
+              selectedValue={'defult'}
+              onValueChange={(itemValue, itemIndex) => console.log(itemValue)}
+            >
+              <Picker.Item label="Comida" value="food" />
+              <Picker.Item label="Transporte" value="transport" />
+              <Picker.Item label="Saúde" value="health" />
+              <Picker.Item label="Educação" value="education" />
+              <Picker.Item label="Entretenimento" value="entertainment" />
+              <Picker.Item label="Compras" value="shopping" />
+              <Picker.Item label="Outros" value="others" />
+              <Picker.Item label="Salário" value="salary" />
+              <Picker.Item label="Freelance" value="freelance" />
+              <Picker.Item label="Investimentos" value="investments" />
+              <Picker.Item label="Negócios" value="business" />
+              <Picker.Item label="Outros Rendimentos" value="other_income" />
+            </Picker>
+          </SelectBoxView>
+        </SelectBoxContainer>
+        <SelectBoxContainer>
+          <SelectBoxLabel>Carteira</SelectBoxLabel>
+          <SelectBoxView>
+            <Picker
+              selectedValue={'defult'}
+              onValueChange={(itemValue, itemIndex) => console.log(itemValue)}
+            >
+              <Picker.Item label="Nubank" value="1" />
+              <Picker.Item label="Mercado Pago" value="2" />
+              <Picker.Item label="Inter" value="3" />
+              <Picker.Item label="PicPay" value="4" />
+              <Picker.Item label="Banco do Brasil" value="5" />
+            </Picker>
+          </SelectBoxView>
+        </SelectBoxContainer>
+        <Input
+          placeholder="Descrição"
+          marginTop={`${hp('3%')}px`}
+        />
+        <SubmitButton>
+          <SubmitButtonText>Enviar</SubmitButtonText>
+        </SubmitButton>
+      </Container>
+    </KeyboardAwareScrollView>
   );
 };
